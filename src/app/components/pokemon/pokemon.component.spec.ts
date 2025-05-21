c6320242354c0338c38dda414ae9e50ef584c460
wutdav <wut.dav@gmail.com>
2025-05-21 15:31:24 +0200

    screw this front end stuff

--- src/app/components/pokemon/pokemon.component.spec.ts
+++ src/app/components/pokemon/pokemon.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PokemonComponent } from './pokemon.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('PokemonComponent', () => {
   let component: PokemonComponent;
@@ -8,9 +10,12 @@ describe('PokemonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PokemonComponent]
+      imports: [PokemonComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: { paramMap: of({ get: () => "1" }) } }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(PokemonComponent);
     component = fixture.componentInstance;
@@ -19,5 +24,33 @@ describe('PokemonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return correct sprite URL based on state', () => {
+    component.pokemon = {
+      id: 1,
+      name: 'bulbasaur',
+      sprites: {
+        frontDefault: 'fd.png',
+        frontShiny: 'fs.png',
+        backDefault: 'bd.png',
+        backShiny: 'bs.png'
+      },
+      stats: { hp: 0, attack: 0, defense: 0, speed: 0 },
+      types: []
+    };
+
+    component.showFront = true;
+    component.isShiny = false;
+    expect(component.currentSprite).toBe('fd.png');
+
+    component.isShiny = true;
+    expect(component.currentSprite).toBe('fs.png');
+
+    component.showFront = false;
+    expect(component.currentSprite).toBe('bs.png');
+
+    component.isShiny = false;
+    expect(component.currentSprite).toBe('bd.png');
   });
 });
--- src/app/services/pokemon.service.spec.ts
+++ src/app/services/pokemon.service.spec.ts
@@ -1,16 +1,57 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PokemonService } from './pokemon.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('PokemonService', () => {
   let service: PokemonService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [PokemonService, provideHttpClientTesting()]
+    });
     service = TestBed.inject(PokemonService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
+
+  afterEach(() => httpMock.verify());
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should fetch and transform Pokemon data correctly", () => {
+    const mockResponse = {
+      id: 25,
+      name: 'pikachu',
+      sprites: {
+        front_default: 'front.png',
+        front_shiny: 'front-shiny.png',
+        back_default: 'back.png',
+        back_shiny: 'back-shiny.png'
+      },
+      stats: [
+        { stat: { name: 'hp' }, base_stat: 35 },
+        { stat: { name: 'attack' }, base_stat: 55 },
+        { stat: { name: 'defense' }, base_stat: 40 },
+        { stat: { name: 'speed' }, base_stat: 90 }
+      ],
+      types: [
+        { type: { name: 'electric' } }
+      ]
+    };
+
+    service.getPokemon(25).subscribe(pokemon => {
+      expect(pokemon.name).toBe("pikachu");
+      expect(pokemon.sprites.frontShiny).toBe('front-shiny.png');
+      expect(pokemon.stats.hp).toBe(35);
+      expect(pokemon.types[0].name).toBe('electric');
+      expect(pokemon.types[0].color).toBe('#F7D02C');
+    });
+
+    const req = httpMock.expectOne("https://pokeapi.co/api/v2/pokemon/25");
+    expect(req.request.method).toBe("GET");
+    req.flush(mockResponse);
   });
 });
