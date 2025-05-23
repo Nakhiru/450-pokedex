43f6e35db224905577d802d1b75ccddae97d80a8
Raphaël Da Silva Mendes <raphael.dasilvamendes@nagra.com>
2025-05-23 14:33:29 +0200

    Restore imports

--- src/app/components/pokemon/pokemon.component.spec.ts
+++ src/app/components/pokemon/pokemon.component.spec.ts
@@ -1,3 +1,9 @@
+import { provideHttpClient } from "@angular/common/http";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Router, ActivatedRoute } from "@angular/router";
+import { of } from "rxjs";
+import { PokemonComponent } from "./pokemon.component";
+
 describe('PokemonComponent', () => {
   let component: PokemonComponent;
   let fixture: ComponentFixture<PokemonComponent>;
